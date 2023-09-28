843d7e6cd8ec9c407209873ab587f736fbe1616c
mandalotech <134844048+mandalotech@users.noreply.github.com>
2023-09-28 14:43:45 +0530

    same correction

--- src/components/Slide2.tsx
+++ src/components/Slide2.tsx
@@ -13,7 +13,7 @@ const Slide2 = () => {
                     <div className="w-4/6 ml-20">
                     <div className="c-4 mb-5">- Contact</div>
                     <div className='text-3xl mb-8'>Any Type of Query & Discussion.</div>
-                    <div className='c-4 text-xs'>Discover the magic of collaboration. Let's create something special together.</div>
+                    <div className='c-4 text-xs'>Discover the magic of collaboration. Let&apos;s create something special together.</div>
                     <a href='mailto:farooqvrofficial@gmail.com' className='flex items-center mt-10 cursor-pointer'>
                         <div className='mr-3 inline c-3 underline peer'>farooqvrofficial@gmail.com</div>
                         <img className='inline mr-4' src="arrow.png" alt="" />
@@ -29,7 +29,7 @@ const Slide2 = () => {
 
                     <div className="w-4/6">
                     <div className='text-2xl my-4'>You can&apos;t use up creativity, the more you use, more you have in the signifant mind</div>
-                    <div className='c-4 text-xs'>Creativity isn't a finite resource; it grows and flourishes the more you tap into it. In the realm of a curious mind, every endeavor sparks even greater imagination and potential</div>
+                    <div className='c-4 text-xs'>Creativity isn&apos;t a finite resource; it grows and flourishes the more you tap into it. In the realm of a curious mind, every endeavor sparks even greater imagination and potential</div>
                     <a href='/resume' className='flex space-x-10 items-center mt-10 cursor-pointer'>
                         <div className=" flex items-center">
                             <div className='text-5xl mr-4 c-3'>1</div>
