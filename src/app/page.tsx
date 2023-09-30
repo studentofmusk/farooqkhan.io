d6a7770874fd5ceba3c18523ac3175d70a9a3233
mandalotech <134844048+mandalotech@users.noreply.github.com>
2023-09-30 12:44:08 +0530

    after adding Footer and slide4 empty template

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,10 @@
 import Boxes from "@/components/Boxes";
+import Footer from "@/components/Footer";
 import Navbar from "@/components/Navbar";
 import Slide1 from "@/components/Slide1";
 import Slide2 from "@/components/Slide2";
 import Slide3 from "@/components/Slide3";
+import Slide4 from "@/components/Slide4";
 
 export default function Home() {
   return (
@@ -13,6 +15,8 @@ export default function Home() {
     <Slide2/>
     <Boxes/>
     <Slide3/>
+    <Slide4/>
+    <Footer/>
     
     
 
---
+++ src/components/Slide4.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Slide4 = () => {
+  return (
+    <section className='h-80 bg-1'></section>
+  )
+}
+
+export default Slide4
