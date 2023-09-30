598870b980fdc4adf3313bb7ccde8271435d59aa
mandalotech <134844048+mandalotech@users.noreply.github.com>
2023-09-30 18:32:09 +0530

    after setup slide4 Profile and quote

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,12 +4,12 @@ const Footer = () => {
   return (
     <section className='flex flex-col justify-between items-center'>
         <img className='w-32' src="logo.png" alt="" />
-        <div className='mb-16 font-bold text-sm'>Thanks for scrolling, <span className='font-normal text-slate-300'>That&apos;s all!</span> </div>
+        <div className='mb-16 font-bold text-sm'>Thanks for scrolling, <span className='font-normal text-slate-300'>That&apos;s all</span> </div>
         <div className='mb-10 flex space-x-10 cursor-pointer'>
                     
-                    <a className='hover:-translate-y-1 duration-150' target='_blank' href="https://github.com/studentofmusk"><img src="GitHub.png" alt=""  /></a>
-                    <a className='hover:-translate-y-1 duration-150' target='_blank' href="https://instagram.com/student_of_musk?igshid=OGQ5ZDc2ODk2ZA=="><img src="Instagram.png" alt=""  /></a>
-                    <a className='hover:-translate-y-1 duration-150' target='_blank' href="https://www.linkedin.com/in/farooq-khan-89a67a252"><img src="LinkedIn.png" alt=""  /></a>
+                    <a className='hover:scale-125 duration-150' target='_blank' href="https://github.com/studentofmusk"><img src="GitHub.png" alt=""  /></a>
+                    <a className='hover:scale-125 duration-150' target='_blank' href="https://instagram.com/student_of_musk?igshid=OGQ5ZDc2ODk2ZA=="><img src="Instagram.png" alt=""  /></a>
+                    <a className='hover:scale-125 duration-150' target='_blank' href="https://www.linkedin.com/in/farooq-khan-89a67a252"><img src="LinkedIn.png" alt=""  /></a>
         </div>
     </section>
   )
--- src/components/Slide4.tsx
+++ src/components/Slide4.tsx
@@ -2,7 +2,22 @@ import React from 'react'
 
 const Slide4 = () => {
   return (
-    <section className='h-80 bg-1'></section>
+    <section className='flex py-20 items-start justify-center bg-1'>
+        <div className='w-72 h-96 mr-20' id='profile'></div>
+        <div className='w-3/6 text-left' >
+        <span><svg className=' mb-5 w-14 fill-[#434657]' viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path d="m0 0h24v24h-24z" fill="none"/><path d="m4.583 17.321c-1.03-1.094-1.583-2.321-1.583-4.31 0-3.5 2.457-6.637 6.03-8.188l.893 1.378c-3.335 1.804-3.987 4.145-4.247 5.621.537-.278 1.24-.375 1.929-.311 1.804.167 3.226 1.648 3.226 3.489a3.5 3.5 0 0 1 -3.5 3.5c-1.073 0-2.099-.49-2.748-1.179zm10 0c-1.03-1.094-1.583-2.321-1.583-4.31 0-3.5 2.457-6.637 6.03-8.188l.893 1.378c-3.335 1.804-3.987 4.145-4.247 5.621.537-.278 1.24-.375 1.929-.311 1.804.167 3.226 1.648 3.226 3.489a3.5 3.5 0 0 1 -3.5 3.5c-1.073 0-2.099-.49-2.748-1.179z"/></svg></span>
+        <span className='text-3xl'>Challenges are what make life interesting&#59; overcoming them is what makes life meaningful.</span>
+        <div className='mt-10 text-2xl c-3'>Joshua J. Marine</div>
+        <div className="mt-5 flex space-x-3">
+          <div className='w-3 h-3 rounded-full bg-3 animate-bounce'></div>
+          <div className='w-3 h-3 rounded-full bg-2 animate-bounce '></div>
+          <div className='w-3 h-3 rounded-full bg-3 animate-bounce '></div>
+          <div className='w-3 h-3 rounded-full bg-2 animate-bounce '></div>
+          
+        </div>
+      </div>
+
+    </section>
   )
 }
 
